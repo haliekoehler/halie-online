08982a96ebcc0b4f846587c32acaf9a5f8c0a924
Halie <halie.koehler@cyberfortress.com>
2020-07-08 15:18:27 -0500

    installed and linked anime.js library

---
+++ scripts/main.js
@@ -0,0 +1,15 @@
+'use strict';
+
+(function() {
+
+	// const anime = require('animejs');
+
+	anime({
+	  // targets: 'div',
+	  // translateX: 250,
+	  // rotate: '1turn',
+	  // backgroundColor: '#000',
+	  // duration: 800
+	});
+
+})();
