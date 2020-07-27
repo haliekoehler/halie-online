5e17bc5046b585d69a417e178d1ae3ea046869b0
Halie <halie.koehler@cyberfortress.com>
2020-07-27 13:18:14 -0500

    resume file update, anime.js start with hero tet

--- scripts/main.js
+++ scripts/main.js
@@ -2,12 +2,15 @@
 
 (function() {
 
+	var title = document.getElementById('title');
+
 	anime({
-	  // targets: 'div',
-	  // translateX: 250,
-	  // rotate: '1turn',
-	  // backgroundColor: '#000',
-	  // duration: 800
+	  targets: title,
+	  translateX: 1000,
+		// delay: 0,
+	  duration: 800,
+		direction: 'reverse',
+		easing: 'spring(0, 10, 75, 2)'
 	});
 
 })();
