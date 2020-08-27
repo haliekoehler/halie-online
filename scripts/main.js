677decbab2eec4aac865247993e2e892699bf95a
Halie <halie.koehler@cyberfortress.com>
2020-08-26 22:29:39 -0500

    social icons in level

--- scripts/main.js
+++ scripts/main.js
@@ -2,20 +2,36 @@
 
 (function() {
 
+	var title = document.getElementById('title');
 	var titleTimeline = anime.timeline({
 		easing: 'spring(0, 10, 75, 2)',
 		duration: 1000
 	});
-	
-	var title = document.getElementById('title');
 
-	anime({
-	  targets: title,
-	  translateX: 1000,
-		// delay: 0,
-	  duration: 800,
-		direction: 'reverse',
-		easing: 'spring(0, 10, 75, 2)'
-	});
-
-})();
+// titleTimeline.add({
+// 	targets: title,
+// 	translateX: 1000,
+// 	// delay: 0,
+// 	duration: 800,
+// 	direction: 'reverse',
+// 	easing: 'spring(0, 10, 75, 2)'
+// }).add({
+// 	targets: title,
+// 	translateX: -1000,
+// 	// delay: 0,
+// 	duration: 800,
+// 	direction: 'reverse',
+// 	easing: 'spring(0, 10, 75, 2)'
+// });
+//
+//
+// 	anime({
+// 	  targets: title,
+// 	  translateX: 1000,
+// 		// delay: 0,
+// 	  duration: 800,
+// 		direction: 'reverse',
+// 		easing: 'spring(0, 10, 75, 2)'
+// 	});
+//
+// })();
