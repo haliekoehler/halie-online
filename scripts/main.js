bfeea53ae7c8a1570eacef490b6695f8f90659de
Halie <halie.koehler@cyberfortress.com>
2020-09-24 10:28:11 -0500

    Using TypeIt JS library

--- scripts/main.js
+++ scripts/main.js
@@ -2,36 +2,39 @@
 
 (function() {
 
-	var title = document.getElementById('title');
-	var titleTimeline = anime.timeline({
-		easing: 'spring(0, 10, 75, 2)',
-		duration: 1000
-	});
+new TypeIt("#hero-header", {
+	speed: 45,
+	lifelike: true,
+	html: true
+})
+.type("Hello.")
+.pause(250)
+.type(" I'm <span>Halie Koehler</span>,")
+.pause(500)
+.break()
+.type("a ")
+.pause(60)
+.type("Designer.")
+.pause(200)
+.options({speed:100})
+.delete(9)
+.options({speed: 80})
+.type("UI/UX Designer.")
+.pause(500)
+.delete(1)
+.pause(20)
+.type(",")
+.pause(200)
+.options({speed: 45})
+.type(" Front-end Developer,")
+.pause(200)
+.type(" and")
+.pause(300)
+.type(" Visual Artist.")
+.pause(5000)
+.options({afterComplete: function(step, instance) {
+	instance.destroy();
+}})
+.go();
 
-// titleTimeline.add({
-// 	targets: title,
-// 	translateX: 1000,
-// 	// delay: 0,
-// 	duration: 800,
-// 	direction: 'reverse',
-// 	easing: 'spring(0, 10, 75, 2)'
-// }).add({
-// 	targets: title,
-// 	translateX: -1000,
-// 	// delay: 0,
-// 	duration: 800,
-// 	direction: 'reverse',
-// 	easing: 'spring(0, 10, 75, 2)'
-// });
-//
-//
-// 	anime({
-// 	  targets: title,
-// 	  translateX: 1000,
-// 		// delay: 0,
-// 	  duration: 800,
-// 		direction: 'reverse',
-// 		easing: 'spring(0, 10, 75, 2)'
-// 	});
-//
-// })();
+})();
