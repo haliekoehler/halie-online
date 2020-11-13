f6ac2c153858c4923fe43ed8a3a98ee724ab7116
Halie <halie@halie.online>
2020-11-13 14:02:59 -0600

    messing with a navbar

--- scripts/main.js
+++ scripts/main.js
@@ -1,88 +1,88 @@
-'use strict';
-
-(function() {
-
-let status = 'active';
-
-// // fade out
-// function fadeOut(el){
-//   el.style.opacity = 1;
+// 'use strict';
 //
-//   (function fade() {
-//     if ((el.style.opacity -= .1) < 0) {
-//       el.style.display = 'none';
-//       el.classList.add('hidden');
-//     } else {
-//       requestAnimationFrame(fade);
-//     }
-//   })();
+// (function() {
+//
+// let status = 'active';
+//
+// // // fade out
+// // function fadeOut(el){
+// //   el.style.opacity = 1;
+// //
+// //   (function fade() {
+// //     if ((el.style.opacity -= .1) < 0) {
+// //       el.style.display = 'none';
+// //       el.classList.add('hidden');
+// //     } else {
+// //       requestAnimationFrame(fade);
+// //     }
+// //   })();
+// // }
+// //
+// // // fade in
+// // function fadeIn(el, display){
+// //   if (el.classList.contains('hidden')){
+// //     el.classList.remove('hidden');
+// //   }
+// //   el.style.opacity = 0;
+// //   el.style.display = display || "block";
+// //
+// //   (function fade() {
+// //     var val = parseFloat(el.style.opacity);
+// //     if (!((val += .1) > 1)) {
+// //       el.style.opacity = val;
+// //       requestAnimationFrame(fade);
+// //     }
+// //   })();
+// // }
+//
+// function animationStatus() {
+// 	let el = document.getElementById('links');
+//
+// 	if (status = 'active') {
+// 		status = 'complete'
+// 	}
+// 	// console.log(status);
+// 	if (status = 'complete') {
+// 		fadeIn(el);
+// 	}
 // }
 //
-// // fade in
-// function fadeIn(el, display){
-//   if (el.classList.contains('hidden')){
-//     el.classList.remove('hidden');
-//   }
-//   el.style.opacity = 0;
-//   el.style.display = display || "block";
+// // var btn = document.querySelector('.js-btn');
+// // var el = document.querySelector('.js-fade');
+// //
+// // btn.addEventListener('click', function(e){
+// //   if(el.classList.contains('is-hidden')){
+// //     fadeIn(el);
+// //   }
+// //   else {
+// //     fadeOut(el);
+// //   }
+// // });
 //
-//   (function fade() {
-//     var val = parseFloat(el.style.opacity);
-//     if (!((val += .1) > 1)) {
-//       el.style.opacity = val;
-//       requestAnimationFrame(fade);
-//     }
-//   })();
-// }
-
-function animationStatus() {
-	let el = document.getElementById('links');
-
-	if (status = 'active') {
-		status = 'complete'
-	}
-	// console.log(status);
-	if (status = 'complete') {
-		fadeIn(el);
-	}
-}
-
-// var btn = document.querySelector('.js-btn');
-// var el = document.querySelector('.js-fade');
+// new TypeIt("#hero-header", {
+// 	speed: 45,
+// 	lifelike: true,
+// 	html: true,
+// 	deleteSpeed: 80
+// })
+// .type("Hello.")
+// .pause(250)
+// .type(" I'm <span>Halie Koehler</span>,")
+// .pause(300)
+// .break()
+// .type("a UI/UX Designer,")
+// .pause(200)
+// .options({speed: 45})
+// .type(" Front-end Developer,")
+// .pause(200)
+// .type(" and")
+// .pause(200)
+// .type(" Visual Artist.")
+// .pause(1500)
+// .options({afterComplete: function(step, instance) {
+// 	// animationStatus();
+// 	instance.destroy();
+// }})
+// .go();
 //
-// btn.addEventListener('click', function(e){
-//   if(el.classList.contains('is-hidden')){
-//     fadeIn(el);
-//   }
-//   else {
-//     fadeOut(el);
-//   }
-// });
-
-new TypeIt("#hero-header", {
-	speed: 45,
-	lifelike: true,
-	html: true,
-	deleteSpeed: 80
-})
-.type("Hello.")
-.pause(250)
-.type(" I'm <span>Halie Koehler</span>,")
-.pause(300)
-.break()
-.type("a UI/UX Designer,")
-.pause(200)
-.options({speed: 45})
-.type(" Front-end Developer,")
-.pause(200)
-.type(" and")
-.pause(200)
-.type(" Visual Artist.")
-.pause(1500)
-.options({afterComplete: function(step, instance) {
-	// animationStatus();
-	instance.destroy();
-}})
-.go();
-
-})();
+// })();
